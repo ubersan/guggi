93f5f9eb8455dd723d1987dfac47e2e5bbeda018
geischtli <sanhuber@student.ethz.ch>
2017-03-11 10:59:03 +0100

    some functionality added to new round form

--- src/app/guggi-start.component.ts
+++ src/app/guggi-start.component.ts
@@ -14,10 +14,24 @@ export class GuggiStartComponent {
     curr_desc:string = '';
     curr_text:string = '';
 
+    selectors = ['Der/Das', 'Jede', 'Alle'];
+    curr_selector = '';
+
+    colors = ['Rose', 'Eichel', 'Schilte', 'Schelle'];
+    curr_color = '';
+
+    cards = ['6', '7', '8', '9', 'Banner', 'Under', 'Ober', 'König', 'Ass'];
+    curr_card = '';
+
+    curr_cost = '';
+
     temp_descs: string[] = [];
 
     done(): void {
-        this.temp_descs.push(this.curr_text + this.curr_desc);
-        this.curr_desc = '';
+        this.temp_descs.push(this.curr_selector + ' ' + this.curr_color + ' ' + this.curr_card + ' kostet ' + this.curr_cost + ' CHF');
+        this.curr_selector = '';
+        this.curr_color = '';
+        this.curr_card = '';
+        this.curr_cost = '';
     }
 }
